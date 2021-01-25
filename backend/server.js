491f93ba5da1996c8d2526d19f87b58bc1fe8c65
Errol <christopherlugod@gmail.com>
2021-01-25 20:44:14 +0800

    first attempt

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import data from './data.js';
+const app= express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/products', (req, res)=>{
+    res.send(data.products);
+})
+app.get('/', (req, res)=>{
+    res.send('server is ready');
+});
+
+app.listen(port, ()=>{
+    console.log('serve at http://localhost:${port}');
+});
