a2cef98d017be98d8a90d33742066e0090f5ebf7
Errol <christopherlugod@gmail.com>
2021-01-26 22:21:31 +0800

    session 20 done

--- backend/data.js
+++ backend/data.js
@@ -3,8 +3,8 @@ import bcrypt from 'bcryptjs';
 const data= {
    users: [
       {
-        name: 'mabuX',
-        email: 'admin@example.com',
+        name: "Errol1005",
+        email: "capistranoerrol05@gmail.com",
         password: bcrypt.hashSync('1234', 8),
         isAdmin: true,
       },
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import {BrowserRouter, Link, Route} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
 
 function App() {
 
   //Adding of badge
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
 
   return (
     <BrowserRouter>
@@ -17,7 +24,7 @@ function App() {
     <header className="row">
         <div>
         <Link className="brand" to="/">
-        Marketplace
+        Home Products
         </Link>
         </div>
         <div>
@@ -27,7 +34,22 @@ function App() {
           <span className="badge">{cartItems.length}</span>
         )}
       </Link>
-      <Link to="/signin">Sign In</Link>
+      {userInfo ? (
+        <div className="dropdown">
+          <Link to="#">
+            {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+          </Link>
+          <ul className="dropdown-content">
+            <li>
+              <Link to="#signout" onClick={signoutHandler}>
+                Sign Out
+              </Link>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <Link to="/signin">Sign In</Link>
+      )}
         </div>
     </header>
     <main>
