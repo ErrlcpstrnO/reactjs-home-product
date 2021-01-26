4190e14204ff2683e9ad2064148cc9ebeccd9ae3
Errol <christopherlugod@gmail.com>
2021-01-26 21:21:31 +0800

    session 18 done

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,20 @@ import express from 'express';
 import mongoose from 'mongoose';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 const app= express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/ProductHome', {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/marketplace', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
+
+
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
